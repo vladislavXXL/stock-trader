035d9fc9029efdf0513cc29f294dfb2b772b9186
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-02-12 02:08:30 +0300

    Setup form

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,105 @@
 <template>
     <div class="container">
-        <app-header></app-header>
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sn-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input type="text" id="email" class="form-control" v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="text" id="password" class="form-control" v-model="userData.password">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input type="number" id="age" class="form-control" v-model="userData.age">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sn-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label>
+                    <textarea id="message" class="form-control" rows="5"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sn-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input type="checkbox" id="sendmail" value="SendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input type="checkbox" id="sendInfomail" value="SendInfoMail"> Send Infomail
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sn-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input type="radio" id="male" value="Male"> Male
+                    </label>
+                    <label for="female">
+                        <input type="radio" id="female" value="Female"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sn-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="priority">Priority</label>
+                    <select id="priority" class="form-control">
+                        <option value=""></option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sn-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <button class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
         <hr>
         <div class="row">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
+            <div class="col-xs-12 col-sm-8 col-sn-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p>Message: </p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                        <p>Gender: </p>
+                        <p>Priority: </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-    import Header from "./components/Shared/Header.vue";
-    import Footer from "./components/Shared/Footer.vue";
-    import Servers from "./components/Server/Servers.vue";
-    import ServerDetails from "./components/Server/ServerDetails.vue";
-
     export default {
-        components: {
-            'app-header': Header,
-            'app-servers': Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
+        data() {
+            return {
+                userData: {
+                    email: '',
+                    password: '',
+                    age: 27
+                },
+            }
         }
     }
-
 </script>
 
 <style scoped>
-
 </style>
