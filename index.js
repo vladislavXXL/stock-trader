95f63bdb4ac85e6984f0f6b4c21cf8e69d73612b
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-03-03 00:08:46 +0300

    Upd with inpterceptors

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ import VueResource from 'vue-resource';
 import App from './src/App.vue'
 
 Vue.use(VueResource);
-Vue.http.options.root = 'localhost';
+Vue.http.options.root = 'http://localhost';
+Vue.http.interceptors.push((request, next) => {
+    console.log(request);
+    if (request.method === 'POST') {
+        request.method = 'PUT';
+    }
+    next(response => {
+        response.json = () => {
+            return {messages: response.body};
+        }
+    });
+});
 
 new Vue({
     el: "#app",
     render: h => h(App)
 });
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
