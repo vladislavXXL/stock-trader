6b2bd88ea650d409164a9543d253475cd38c554b
Viacheslav Kupriyanov <vkupriyano@wiley.com>
2020-03-04 14:51:26 +0300

    Upd with template URLs

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
+                <input class="form-control" type="text" v-model="node">
+                <br>
                 <button class="btn btn-primary" @click="fetchData">Get data</button>
                 <br><br>
                 <ul class="list-group">
@@ -36,7 +38,8 @@
                     email: ''
                 },
                 users: [],
-                resource: {}
+                resource: {},
+                node: 'data'
             };
         },
         methods: {
@@ -58,7 +61,22 @@
                 this.resource.saveAlt(this.user);
             },
             fetchData() {
+/*
+                // using resource with http requests
                 this.$http.get('data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (let key in data) {
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+                    });
+*/
+                // using resource with parameters
+                this.resource.getData({node: this.node})
                     .then(response => {
                         return response.json();
                     })
@@ -73,9 +91,10 @@
         },
         created() {
             const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'}
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
             };
-            this.resource = this.$resource('data.json', {}, customActions);
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 
