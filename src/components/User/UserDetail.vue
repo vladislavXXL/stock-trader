058a6c4d53e390e1a030f04a51cbbf9c74e213df
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-03-22 21:05:17 +0300

    Dynamic router links

--- src/components/User/UserDetail.vue
+++ src/components/User/UserDetail.vue
@@ -2,6 +2,7 @@
     <div>
         <h3>Some User Details</h3>
         <p>User ID: {{ $route.params.id }}</p>
+        <router-link tag="button" class="btn btn-primary" :to="'/user/' + $route.params.id + '/edit'">Edit User</router-link>
     </div>
 </template>
 
