6014763fb81e59072f594b4917fa4e9edb89b435
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-03-22 20:51:40 +0300

    Navigating to nested routes

--- src/components/User/UserDetail.vue
+++ src/components/User/UserDetail.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <h3>Some User Details</h3>
-        <p>User ID: {{ id }}</p>
+        <p>User ID: {{ $route.params.id }}</p>
     </div>
 </template>
 
 <script>
     export default {
         name: "UserDetail",
-        data() {
+/*        data() {
             return {
                 id: this.$route.params.id
             }
@@ -17,7 +17,7 @@
             '$route'(to, from) {
                 this.id = to.params.id;
             }
-        }
+        }*/
     }
 </script>
 
