e8f1c1cd1f9ca169083da0ffacf8aef1afa67c88
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-03-25 02:29:40 +0300

    Multiple router views

--- routes.js
+++ routes.js
@@ -3,11 +3,20 @@ import Home from "./src/components/Home.vue";
 import UserStart from "./src/components/User/UserStart.vue";
 import UserEdit from "./src/components/User/UserEdit.vue";
 import UserDetail from "./src/components/User/UserDetail.vue";
+import Header from "./src/components/Shared/Header.vue";
 
 export const routes = [
-    {path: '', component: Home, name: 'home'},
     {
-        path: '/user/', component: User, children: [
+        path: '', name: 'home', components: {
+            default: Home,
+            'header-top': Header
+        }
+    },
+    {
+        path: '/user/', components: {
+            default: User,
+            'header-bottom': Header
+        }, children: [
             {path: '', component: UserStart},
             {path: ':id', component: UserDetail},
             {path: ':id/edit', component: UserEdit, name: 'userEdit'}
