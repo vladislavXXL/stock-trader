81ef724b7364c177e666c7607474fbacc24b9702
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-03-25 02:19:03 +0300

    Using query parameters

--- src/components/User/UserDetail.vue
+++ src/components/User/UserDetail.vue
@@ -5,7 +5,7 @@
         <router-link
                 tag="button"
                 class="btn btn-primary"
-                :to="{ name: 'userEdit', params: { id: $route.params.id }}"
+                :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 }}"
         >Edit User</router-link>
     </div>
 </template>
--- src/components/User/UserEdit.vue
+++ src/components/User/UserEdit.vue
@@ -1,5 +1,9 @@
 <template>
-    <h3>Edit the User</h3>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Locale: {{ $route.query.locale }}</p>
+        <p>Analytics: {{ $route.query.q }}</p>
+    </div>
 </template>
 
 <script>
