66d5a773bd575a192d19d7c0687a0e05f4219387
Viacheslav Kupriyanov <vitya39@gmail.com>
2020-04-10 22:57:15 +0300

    Two-way binding and Vuex

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
                 <hr>
                 <app-counter></app-counter>
                 <app-another-counter></app-another-counter>
-
+                <hr>
+                <!--<input type="text" :value="value" @input="updateValue">-->
+                <input type="text" v-model="value">
+                <p>{{value}}</p>
             </div>
         </div>
     </div>
@@ -19,7 +22,25 @@
     import Result from "./elements/Result.vue";
     import AnotherResult from "./elements/AnotherResult.vue";
     import AnotherCounter from "./elements/AnotherCounter.vue";
+
     export default {
+        computed: {
+            value: {
+                get() {
+                    return this.$store.getters.value;
+                },
+                set(value) {
+                    this.$store.dispatch('updateValue', value);
+                }
+            }
+        },
+/*
+        methods: {
+            updateValue(event) {
+                this.$store.dispatch('updateValue', event.target.value)
+            }
+        },
+*/
         components: {
             appCounter: Counter,
             appAnotherCounter: AnotherCounter,
